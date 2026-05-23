cdc14e3a22e7b0534f180d9f7a25872bb8915261
Gustavo Onaga <gustavo.onaga1@gmail.com>
2026-05-23 17:35:43 -0300

    fix: Ajuste dos projetos, certificacoes e formacaoa

--- js/carousel.js
+++ js/carousel.js
@@ -1,1 +1,7 @@
-console.log("Carousel pronto para expansão futura.");
+function scrollCarousel(direction) {
+    const carousel = document.getElementById("cert-carousel");
+    carousel.scrollBy({
+        left: direction * 300,
+        behavior: "smooth"
+    });
+}
